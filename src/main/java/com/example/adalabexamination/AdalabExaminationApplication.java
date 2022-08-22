fa060e736522186c84aee7d3d6e1e6b4e7c491a7
study <1531621783@qq.com>
2022-08-22 12:24:42 +0800

    1

---
+++ src/main/java/com/example/adalabexamination/AdalabExaminationApplication.java
@@ -0,0 +1,13 @@
+package com.example.adalabexamination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdalabExaminationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdalabExaminationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/adalabexamination/AdalabExaminationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adalabexamination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdalabExaminationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
