3f59c282cb672017f35a867e7b9f5b2b868e994a
karl-von <99388708@qq.com>
2022-08-23 10:39:19 +0800

    @GetMapping("getRanking")

--- src/main/java/com/adalab/examination/controller/StudentController.java
+++ src/main/java/com/adalab/examination/controller/StudentController.java
@@ -1,11 +1,17 @@
 package com.adalab.examination.controller;
 
 
+import com.adalab.examination.entity.Student;
+import com.adalab.examination.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,12 +22,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-08-22
  */
 @RestController
-@RequestMapping("/examination/student")
+@RequestMapping("/api/student")
 @CrossOrigin
 public class StudentController {
+    @Autowired
+    StudentService studentService;
+
     @GetMapping("/ping")
     public String ping(){
         return "pong";
+    }
+
+    @GetMapping("getRanking")
+    public List<Student> getRanking(){
+        return studentService.list();
     }
 
 }
