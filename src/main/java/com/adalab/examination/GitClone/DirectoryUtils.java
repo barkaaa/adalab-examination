e02cd510b2d47dd13d857ec4a30fe0555919b30a
Ayaya33 <hzh553871310@163.com>
2022-08-25 10:19:36 +0800

    提供一个接口 可以获取学员代码提交记录

---
+++ src/main/java/com/adalab/examination/GitClone/DirectoryUtils.java
@@ -0,0 +1,31 @@
+package com.adalab.examination.GitClone;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+public class DirectoryUtils {
+    public static HashMap<String, Object> traverseDir(String path){
+        HashMap<String, Object> map = Maps.newHashMap();
+        File file = new File(path);
+        if (file.isDirectory()){
+            File[] files = file.listFiles();
+            List<Object> list = Lists.newArrayList();
+            if (Objects.nonNull(files)){
+                for (File value : files) {
+                    if (!value.isDirectory()) {
+                        list.add(value.getName());
+                    } else if (!value.getName().endsWith(".git")){
+                        list.add(traverseDir(value.getAbsolutePath()));
+                    }
+                }
+                map.put(file.getName(), list);
+            }
+        }
+        return map;
+    }
+
+
+
+}
