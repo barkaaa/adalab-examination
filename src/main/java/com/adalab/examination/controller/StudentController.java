1e4e49d52f2eb3c4c91ccd98d93d3e74329aa210
Ayaya33 <hzh553871310@163.com>
2022-08-25 10:16:47 +0800

    提供一个接口 可以获取学员代码提交记录

--- src/main/java/com/adalab/examination/controller/StudentController.java
+++ src/main/java/com/adalab/examination/controller/StudentController.java
@@ -1,24 +1,27 @@
 package com.adalab.examination.controller;
 
 
+import com.adalab.examination.GitClone.DirectoryUtils;
 import com.adalab.examination.entity.Student;
 import com.adalab.examination.service.StudentService;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.File;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
+import static com.adalab.examination.GitClone.DirectoryUtils.traverseDir;
 import static com.adalab.examination.GitClone.GitUtils.delAllFile;
+import static com.adalab.examination.GitClone.GitUtils.getFiles;
 
 /**
  * <p>
@@ -67,10 +70,19 @@ public class StudentController {
     //在resources文件夹下根据学员姓名生成文件夹 clone学员的代码
     @PostMapping("/studentCode")
     public String cloneCode(@RequestBody Map<String,String>map){
+
         String gitURL = map.get("url");
         String path = System.getProperty("user.dir");
         String name = "/"+map.get("name");
-        String localPath = path+"/src/main/resources/studentCode"+name;//+studentName
+        String pattern = "MM-dd-HH:mm:ss";
+        //获取当地时区
+        Locale locale = Locale.getDefault();
+        //利用SimpleDateFormat 进行时间格式的转换
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern,locale);
+//        String time = "/"+sdf.format(new Date());
+        String time = "/"+System.currentTimeMillis();
+
+        String localPath = path+"/src/main/resources/studentCode"+name+time;//+studentName
         File file = new File(localPath);
         if (file.exists()){
             logger.info("删除文件结果:"+delAllFile(localPath));
@@ -81,14 +93,27 @@ public class StudentController {
                     .setURI(gitURL)
                     .setDirectory(new File(localPath))
                     .call();
-            logger.info("clone成功"+git.toString());
+            logger.info("clone成功!"+git.toString());
         } catch (GitAPIException e) {
-            logger.error("clone失败");
+            logger.error("clone失败!");
             e.printStackTrace();
             return "克隆失败";
         }
         return "OK";
     }
+
+
+    //获取学员代码提交历史记录的文件结构树
+    @PostMapping("/studentCode/FilesTree")
+    public Object getFilesTree(){
+
+        String localPath = System.getProperty("user.dir")+"/src/main/resources/studentCode";
+        HashMap<String, Object> hashMap = traverseDir(localPath);
+        JSONObject json = new JSONObject(hashMap);
+        logger.info("获取到结构树:"+ json);
+        return json;
+    }
+
 
 }
 
