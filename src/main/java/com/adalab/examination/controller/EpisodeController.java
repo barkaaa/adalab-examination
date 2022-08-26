c8d37e990cc5dc5dc5d3652d6828b25bd3212a70
barkaaa <liuboyang1531621783@gmail.com>
2022-08-26 15:20:32 +0800

    episode controller 创建

---
+++ src/main/java/com/adalab/examination/controller/EpisodeController.java
@@ -0,0 +1,10 @@
+package com.adalab.examination.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/episode")
+
+public class EpisodeController {
+
+}
