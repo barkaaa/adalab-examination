28b618988f027be30e1f68449e158df43263ebe1
yay <>
2022-08-26 15:22:47 +0800

    docker 更新

--- src/main/java/com/adalab/examination/controller/EpisodeController.java
+++ src/main/java/com/adalab/examination/controller/EpisodeController.java
@@ -1,10 +1,22 @@
 package com.adalab.examination.controller;
 
+import com.adalab.examination.service.DockerService;
+import com.adalab.examination.service.FileUpLoadService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/episode")
 
 public class EpisodeController {
+    DockerService dockerService;
+    FileUpLoadService fileUpLoadService;
+
+
+    EpisodeController(DockerService dockerService, FileUpLoadService fileUpLoadService) {
+        this.dockerService = dockerService;
+        this.fileUpLoadService = fileUpLoadService;
+
+    }
+
 
 }
