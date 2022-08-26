7bc173c212b69b7cfdea297ac6b6b6b3a0cc484f
yay <>
2022-08-26 15:26:28 +0800

    docker 更新

--- src/main/java/com/adalab/examination/controller/EpisodeController.java
+++ src/main/java/com/adalab/examination/controller/EpisodeController.java
@@ -1,7 +1,10 @@
 package com.adalab.examination.controller;
 
+import com.adalab.examination.entity.TestResult;
 import com.adalab.examination.service.DockerService;
+import com.adalab.examination.service.EpisodeService;
 import com.adalab.examination.service.FileUpLoadService;
+import com.adalab.examination.service.impl.EpisodeServiceImpl;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,12 +13,28 @@ import org.springframework.web.bind.annotation.*;
 public class EpisodeController {
     DockerService dockerService;
     FileUpLoadService fileUpLoadService;
+    EpisodeService episodeService;
 
 
-    EpisodeController(DockerService dockerService, FileUpLoadService fileUpLoadService) {
+    EpisodeController(DockerService dockerService, FileUpLoadService fileUpLoadService, EpisodeService episodeService) {
         this.dockerService = dockerService;
         this.fileUpLoadService = fileUpLoadService;
+        this.episodeService = episodeService;
+    }
 
+    String uploadDockerFile() {
+        //todo
+        return null;
+    }
+
+    String uploadTestFile() {
+        //todo
+        return null;
+    }
+
+    TestResult doTest() {
+        //todo
+        return null;
     }
 
 
