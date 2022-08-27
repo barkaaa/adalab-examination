37a3c7f11178fd7418a50452a459dad5e6c38ac3
zxear <2602388192@qq.com>
2022-08-27 17:51:53 +0800

    zxear

---
+++ src/main/java/com/adalab/examination/Docker/DockerImageFactoryBean.java
@@ -0,0 +1,33 @@
+package com.adalab.examination.Docker;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.BuildImageResultCallback;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+
+@Component
+public class DockerImageFactoryBean {
+    private final  DockerClient client;
+
+    DockerImageFactoryBean(DockerClient client) {
+        this.client = client;
+
+    }
+
+    public String createImage(String fileName, Set<String> tags) throws IOException {
+        File file = new File("src/main/resources");
+        String resourcePath = file.getCanonicalPath();
+        File stuFileRepo = new File(resourcePath + "/dockerFiles/" + fileName);
+
+
+        return client.buildImageCmd(stuFileRepo)
+                .withTags(tags)
+                .exec(new BuildImageResultCallback())
+                .awaitImageId();
+    }
+
+
+}
