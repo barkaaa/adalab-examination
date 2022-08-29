68079828e62973fb9a8fcfcd9bbf66ee952767dd
雄霸天下 <bighubaba16@outlook.com>
2022-08-29 17:59:08 +0800

    episode 去掉自增

--- src/main/java/com/adalab/examination/entity/Episode.java
+++ src/main/java/com/adalab/examination/entity/Episode.java
@@ -25,7 +25,7 @@ public class Episode implements Serializable {
 
 
     private static final long serialVersionUID = 1L;
-    @TableId(value = "id", type = IdType.AUTO)
+
     private Integer id;
 
     private String testFileName;
